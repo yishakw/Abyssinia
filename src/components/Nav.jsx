7cf12d1eff895934803cabb015dccf4a2f23487b
yishak wondimgezahu <yishakwondimgezahu12@gmail.com>
2024-08-01 17:22:36 +0300

    update on the apperance of the nav dropdown menu for mobile and tablet

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,54 +4,47 @@ import Button from "./Button";
 import IMAGES from "../assets/Images";
 import { useSelector, useDispatch } from "react-redux";
 import { increment } from "../state/slices/counterSlice";
+import { useState } from "react";
 
 export default function Nav() {
   // const count = useSelector((state) => state.counter.value)
   // const dispatch = useDispatch()
-  
+  const [show, setShow] = useState(false)
+ 
+  console.log(show)
   return (
     <nav className="flex sticky justify-between items-center bg-sky-400 bg-opacity-10  sm:px-6 sm:bg-slate-10 p-6 text-xl ">
       <div>
-    <img src={IMAGES.logo} alt="logo" className="relative rounded-full bg-whit bg-opacity-40 w-[100px]" />
-    <div className="absolute top-11 rounded-3xl bottom-0 right-0 left-8 flex items-center justify-center w-20 -z-10 tw-11 h-11 opacity-35 -rotate-12 bg-black "></div>
+        <img
+          src={IMAGES.logo}
+          alt="logo"
+          className="relative rounded-full bg-whit bg-opacity-40 w-[100px]"
+        />
+        <div className="absolute  top-11 rounded-3xl bottom-0 right-0 left-8 flex items-center justify-center w-20 -z-10 tw-11 h-11 opacity-35 -rotate-12 bg-black "></div>
       </div>
-      {/* <button>Start Contact</button> */}
-    <div className="hidden lg:flex">
-    <NavBtn title="Home"/>
-    <NavBtn title="Service"/>
-    <NavBtn title="Projects"/>
-    <NavBtn title="FAQs"/>
-    <NavBtn title="Blog"/>
-    </div>
-    <button>
-      </button>
-      <div className="hidden md:block">
+      <div
+        className={`${
+          !show && "hidden"
+        } etfili  absolute  items-center justify-cetner top-28 lg:static  lg:flex`}
+      >
+        <ul className="flex flex-col w-[90vw] lg:w-fit bg-black lg:bg-transparent bg-opacity-75 lg:flex-row gap-3 sm:gap-0 ">
+          <NavBtn title="Home" />
+          <NavBtn title="Service" />
+          <NavBtn title="Projects" />
+          <NavBtn title="FAQs" />
+          <NavBtn title="Blog" /> 
+        </ul>
+      </div>
 
-      {/* <Button name="Start Contact"/> */}
-      <NavBtn title="Start Contact"/>
+      <div className="hidden md:block">
+        <NavBtn title="Start Contact" />
       </div>
-    <BsList size={40} className="lg:hidden"/>
-      {/* <ul>
-        <li>
-      <a href="#">Home</a>
-        </li>
-        <li>
-     <a href="#">Building System</a>
-        </li>
-        <li>
-     <a href="#">Erp System</a>
-        </li>
-        <li>
-     <a href="#">School System</a>
-        </li>
-        <li>
-     <a href="#">projects</a>
-        </li>
-        <li>
-        </li>
-    <a href="#">Blogs</a>
-    <a href="#">FAQa</a>
-      </ul> */}
-  </nav>
-  )
+
+      <BsList
+        size={40}
+        className="lg:hidden text-amber-600"
+        onClick={() => setShow((prev) => !prev)}
+      />
+    </nav>
+  );
 }
--- src/components/NavBtn.jsx
+++ src/components/NavBtn.jsx
@@ -1,11 +1,15 @@
-
-function NavBtn({title}) {
+function NavBtn({ title }) {
   return (
-    <>
-      <a href="#" className="rounded-md hover:border-white hover:border-solid hover:border-opacity-15 hover:border-2 bg-cyan-60 hover:bg-cyan-40  text-amber-300 hover:text-amber-800 px-5 mx-3 py-3 text-sm font-medium" aria-current="page">{title}</a>
-      {/* <a href="#" className=" sm:hidden block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a> */}
-    </>
-  )
+    <li className="list-none mx-3 flex justify-center w-[95%] lg:w-fit lg:my-0 cursor-pointer border-b border-transparent hover:border-b hover:border-slate-500 hover:border-opacity-70 md:hover:border-transparent ">
+      <a
+        href="#"
+        className="rounded-md bg-opacity-45 border border-transparent lg:border lg:hover:scale-110 lg:hover:border-orange-200 hover:border-solid lg:hover:border-opacity-15 hover:border hover:bg-opacity-50 lg:active:scale-95 text-amber-300 hover:text-amber-800 px-5 mx-3 py-3 text-sm font-medium"
+        aria-current="page"
+      >
+        {title}
+      </a>
+    </li>
+  );
 }
 
-export default NavBtn
+export default NavBtn;
