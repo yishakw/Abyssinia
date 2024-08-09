23b541716022ae1fee0f7653738cf718186db08f
yishak wondimgezahu <yishakwondimgezahu12@gmail.com>
2024-08-09 14:58:52 +0300

    updated on the axios fetch on the blogs page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,14 +3,14 @@ import PageContainer from "../components/PageContainer";
 import axios from "axios";
 import BlogsCard from "../components/BlogsCard";
 function Blog() {
-  useEffect(() => {
-    axios
-      .get(
-        `https://www.googleapis.com/blogger/v3/blogs/2399953?key="AIzaSyAAte1YQtqobEmlc7EQwbVSb7yXF2-WNeo";`
-      )
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       `https://www.googleapis.com/blogger/v3/blogs/2399953?key="AIzaSyAAte1YQtqobEmlc7EQwbVSb7yXF2-WNeo";`
+  //     )
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.error(err));
+  // }, []);
   return (
     <PageContainer
       title="Our Blogs"
